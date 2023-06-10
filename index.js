7d10c83ed65f52869a4d1f2265f48710f15bd2b9
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2023-06-10 23:00:11 +0600

    mongodb connected and get appointment Options

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +9,35 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// mongodb connection
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aydlau6.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const appointmentOptionCollection = client
+      .db("doctorsCare")
+      .collection("appointmentOptions");
+    const bookingsCollection = client.db("doctorsCare").collection("bookings");
+
+    app.get("/appointmentOptions", async (req, res) => {
+      const query = {};
+      const options = await appointmentOptionCollection.find(query).toArray();
+      res.send(options);
+    });
+  } finally {
+  }
+}
+run().catch(console.log);
 
 app.get("/", async (req, res) => {
   res.send("Doctors care server is running");
